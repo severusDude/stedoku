ea690c1d8da8c86d2cea8a58468cc3690a58be0e
Lutfi Fajar <60343449+severusDude@users.noreply.github.com>
2025-05-23 10:48:10 +0700

    feat (routes): add sidebar routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-import { Button } from "@/components/ui/button";
+import { AppSidebar } from "@/components/app-sidebar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./components/dashboard/Dashboard";
 import Home from "./components/home/Home";
+import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar";
 
 function App() {
 	return (
-		<Router>
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/dashboard" element={<Dashboard />} />
-			</Routes>
-		</Router>
+		<SidebarProvider>
+			<AppSidebar />
+			<SidebarInset>
+				<Router>
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/dashboard" element={<Dashboard />} />
+					</Routes>
+				</Router>
+			</SidebarInset>
+		</SidebarProvider>
 	);
 }
 
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,26 +1,9 @@
-import { AppSidebar } from "@/components/app-sidebar";
-import {
-	Breadcrumb,
-	BreadcrumbItem,
-	BreadcrumbLink,
-	BreadcrumbList,
-	BreadcrumbPage,
-	BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb";
-import { Separator } from "@/components/ui/separator";
-import {
-	SidebarInset,
-	SidebarProvider,
-	SidebarTrigger,
-} from "@/components/ui/sidebar";
+import React from "react";
 
 function Home() {
 	return (
 		<>
-			<SidebarProvider>
-				<AppSidebar />
-				<SidebarInset></SidebarInset>
-			</SidebarProvider>
+			<p>test</p>
 		</>
 	);
 }
